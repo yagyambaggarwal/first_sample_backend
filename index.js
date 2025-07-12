39ce0e0c8fd36216917d29a521758dfbd4aa62e0
yagyam <vishuaggarwal219@gmail.com>
2025-07-12 14:28:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res)=>{
+    res.send("Hello beta me yhan thik nhin hun");
+})
+
+app.get("/login", (req, res)=>{
+    res.send("This is the login page.")
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`The app is live at ${port}`);
+})
+
+
+
